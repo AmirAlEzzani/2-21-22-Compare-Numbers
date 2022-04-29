3cee565b7d79cda44d7228c7482e5b7d74e1e057
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2022-04-29 01:53:51 -0700

    Add files via upload

---
+++ first Python program.py
@@ -0,0 +1,11 @@
+a = 0
+b = 0
+print("Number comparer")
+a = input("First number: ")
+b = input("Second number: ")
+if a > b:
+    print(a+" is greater than "+b)
+elif b > a:
+    print(b+" is greater than "+a)
+elif a == b:
+    print(a+" is equal to "+b)
